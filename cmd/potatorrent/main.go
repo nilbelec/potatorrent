40f46c3d3a6b8399f759d5a149dd9afbdfc9e4d5
Juan Álvarez <jalvarezcurcho@gmail.com>
2019-12-10 20:56:06 +0100

    Adds PORT environment variable to change the webapp port

--- cmd/potatorrent/main.go
+++ cmd/potatorrent/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/nilbelec/potatorrent/pkg/crawler"
 	"github.com/nilbelec/potatorrent/pkg/github"
 	"github.com/nilbelec/potatorrent/pkg/scheduler"
@@ -9,12 +13,17 @@ import (
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("No PORT environment variable found. Defaulting to port %s\n", port)
+	}
 	crawler := crawler.NewCrawler()
 	github := github.NewClient("nilbelec", "potatorrent")
 	storage := scheduler.NewSchedulesFile("schedules.json")
 	scheduler := scheduler.NewScheduler(crawler, storage)
 
 	server := web.NewServer(crawler, github, scheduler)
-	browser.OpenURL("http://localhost:8080")
-	server.Start(":8080")
+	browser.OpenURL(fmt.Sprintf("http://localhost:%s", port))
+	server.Start(fmt.Sprintf(":%s", port))
 }
