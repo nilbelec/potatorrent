c3bbd577a2db4d9efd2bf9b003025cc6efc1822c
Juan Álvarez <celeblin@gmail.com>
2020-06-21 18:47:03 +0200

    Adds svelte

---
+++ front/src/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Icon from "svelte-awesome";
+  import { clockO } from "svelte-awesome/icons";
+</script>
+
+<style>
+  nav {
+    box-shadow: 0px 2px 5px #000;
+    color: #f8f9fa;
+    padding: 0.5rem;
+    background-color: #343a40;
+  }
+</style>
+
+<nav>
+  <Icon data={clockO} />
+</nav>
